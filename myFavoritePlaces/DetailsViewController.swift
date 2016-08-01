ba9cccc283e75e487650834ae442987ab9501cc8
aabato <angelica.bato@gmail.com>
2016-08-01 19:24:11 -0400

    First commit

---
+++ myFavoritePlaces/DetailsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myFavoritePlaces
+//
+//  Created by Angelica Bato on 8/1/16.
+//  Copyright © 2016 Angelica Bato. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+
+
+}
+
---
+++ myFavoritePlaces/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  myFavoritePlaces
+//
+//  Created by Angelica Bato on 8/1/16.
+//  Copyright © 2016 Angelica Bato. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+struct Location {
+    //Create these properties
+    //1. Name of type String
+    //2. Fun fact of type String
+    //3. ImageNames -- should be an array of strings.
+    //4. Location of type CLLocation
+}
